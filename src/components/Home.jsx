dab229d00c3f49b28aea86791eb4c42aa5f9b8cc
mdaleardc <ismailaliamot@gmail.com>
2025-01-15 21:26:59 +0600

    some update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,10 +6,14 @@ const Home = ({onSearch, data, loading, setError, error}) => {
   return (
     <>
     <div className='px-4 bg-blue-50 py-4 sm:min-h-screen'>
+    <div className='fixed top-0 left-[50%] translate-x-[-50%] bg-zinc-700 w-full max-w-[600px] px-4 sm:px-20'>
     <SearchBar onSearch={onSearch} setError={setError}/>
+    </div>
+    <div className='pt-[2rem]'>
     {
     loading ? <p>Loading...</p> : (error.trim() ? <p>Word not found!</p> : <WordDefinition data={data}/>)
     }
+    </div>
     </div>
     </>
     )
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -14,8 +14,8 @@ const SearchBar = ({onSearch, setError}) => {
   
   return (
     <>
-    <form onSubmit={handleSubmit} className='mx-auto text-center flex flex-row gap-4 justify-between items-center'>
-    <Link to='/about' className='font-bold'>About</Link>
+    <form onSubmit={handleSubmit} className='mx-auto text-center py-2 flex flex-row gap-4 justify-between items-center'>
+    <Link to='/about' className='font-bold text-[#eef]'>About</Link>
     <input type='text' name='word' placeholder="Search word" value={searchInput} className='outline-none border-[2px] focus:ring-[1px] px-2 h-[2.2rem] ring-green-500 rounded-md' onChange={(e)=>{setSearchInput(e.target.value); setError("")}} required/>
     <input type='submit' value="Search" className='px-2 text-white h-[2rem] flex flex-row justify-center items-center rounded-lg bg-blue-500 font-bold'/>
     </form>
